58119828396ba91bb7c78ae37fa3bdbdec63fbe5
Sam Jones <sam@samueljones.codes>
2024-11-05 23:54:32 -0800

    Initial Commit

---
+++ TimeLogger/TimeLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLoggerApp.swift
+//  TimeLogger
+//
+//  Created by Samuel Jones on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeLoggerTests/TimeLoggerTests.swift
@@ -0,0 +1,16 @@
+//
+//  TimeLoggerTests.swift
+//  TimeLoggerTests
+//
+//  Created by Samuel Jones on 11/5/24.
+//
+
+import Testing
+
+struct TimeLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
